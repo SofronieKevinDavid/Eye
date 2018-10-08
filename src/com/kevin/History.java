bc6c8eafc8d5c55d7c20d44c581f2de7647030f7
SofronieDavidKevin <42830854+SofronieKevinDavid@users.noreply.github.com>
2018-10-08 17:03:49 +0300

    Updates

--- src/com/kevin/History.java
+++ src/com/kevin/History.java
@@ -3,6 +3,7 @@ package com.kevin;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class History {
     private int result;
@@ -35,5 +36,20 @@ public class History {
 
     public void setLevel(int level) {
         this.level = level;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof History)) return false;
+        History history = (History) o;
+        return getResult() == history.getResult() &&
+                getLevel() == history.getLevel() &&
+                Objects.equals(getDate(), history.getDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getResult(), getLevel(), getDate());
     }
 }
