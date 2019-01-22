8449019f3489725eb1f68276adc11c5812b8c64b
SofronieKevinDavid <davidsof01@gmail.com>
2019-01-22 14:47:55 +0200

    User made unique by name. No more users with the same name. That's the only change(in user service).

--- src/main/java/com/kevin/service/UserService.java
+++ src/main/java/com/kevin/service/UserService.java
@@ -6,11 +6,6 @@ import com.kevin.domain.User;
 import com.kevin.persistance.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 @Service
 public class UserService {
