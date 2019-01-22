96d2bd12a05bd6295ac02737974a09a859669af8
SofronieKevinDavid <davidsof01@gmail.com>
2019-01-22 21:11:25 +0200

    Merg API-urile perfect, toate. La cursu vitor mai trebuie doar sa umblu la controllere din proiect plus dependencies ceva gen ca sa poata fi accesat de ui din alt server. Dar API-urile din postman merg abolut perfect, e gata.

--- src/main/java/com/kevin/web/UserController.java
+++ src/main/java/com/kevin/web/UserController.java
@@ -5,10 +5,7 @@ import com.kevin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-
 public class UserController {
 
 
