29b3a20a57ed8793c928609ac3f2db078a70f514
SofronieKevinDavid <davidsof01@gmail.com>
2019-01-29 20:24:32 +0200

    API

--- src/main/java/com/kevin/EyeApplication.java
+++ src/main/java/com/kevin/EyeApplication.java
@@ -2,6 +2,10 @@ package com.kevin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -10,5 +14,16 @@ public class EyeApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(EyeApplication.class, args);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
