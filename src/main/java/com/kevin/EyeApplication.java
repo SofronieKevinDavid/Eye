c532cdd61d68b69401e152fc62d90f3ec4f94de3
SofronieKevinDavid <davidsof01@gmail.com>
2019-01-29 20:31:44 +0200

    FINAL UPDATE. ALL GOOD.

--- src/main/java/com/kevin/EyeApplication.java
+++ src/main/java/com/kevin/EyeApplication.java
@@ -15,7 +15,7 @@ public class EyeApplication {
 
         SpringApplication.run(EyeApplication.class, args);
     }
-    
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
